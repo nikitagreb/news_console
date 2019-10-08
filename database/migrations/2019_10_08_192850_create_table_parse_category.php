894d5c6c27933992448f6ac0f2c61475395a6c24
Nikita Grebennikov <nikitagreb0204@mail.ru>
2019-10-08 23:25:09 +0300

    finish

---
+++ database/migrations/2019_10_08_192850_create_table_parse_category.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableParseCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parse_category', function (Blueprint $table) {
+
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->unsignedInteger('category_id')->comment('Категория');
+            $table->unsignedInteger('source_id')->comment('Источник');
+            $table->string('name', 100)->comment('Название');
+            $table->string('link', 250)->comment('Ссылка на страницу категории');
+            $table->string('linkSelector', 250)->comment('Css селектор для ссылки');
+            $table->timestamps();
+
+            $table->primary(['category_id', 'source_id']);
+
+            $table->foreign('category_id')
+                ->references('id')->on('category')
+                ->onDelete('cascade');
+            $table->foreign('source_id')
+                ->references('id')->on('parse_source')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parse_category');
+    }
+}
---
+++ database/migrations/2019_10_08_195852_create_table_parse_link_news.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableParseLinkNews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parse_link_news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('category_id')->comment('Категория');
+            $table->unsignedInteger('source_id')->comment('Источник');
+            $table->text('title')->comment('Заголовок');
+            $table->text('link')->comment('Ссылка');
+            $table->enum('status', ['new', 'loaded'])->default('new')->comment('Статус');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')->on('category')
+                ->onDelete('cascade');
+            $table->foreign('source_id')
+                ->references('id')->on('parse_source')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parse_link_news');
+    }
+}
